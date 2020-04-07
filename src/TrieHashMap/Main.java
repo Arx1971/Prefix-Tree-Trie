1a97f4c4e295d5f3d1cf435bcc1ef12ac78c777d
Arx1971 <adnan.rahin13@gmail.com>
2020-04-07 19:51:54 -0400

    Trie Insertion

--- src/TrieHashMap/Main.java
+++ src/TrieHashMap/Main.java
@@ -22,4 +22,40 @@ class Trie {
         }
     }
 
+    private final TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+
+        TrieNode current = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            TrieNode node = current.map.get(word.charAt(i));
+            if (node == null) {
+                node = new TrieNode();
+                current.map.put(word.charAt(i), node);
+            }
+            current = node;
+        }
+        current.isEndOfTheWord = true;
+    }
+
+    public boolean search(String word) {
+
+        TrieNode current = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            TrieNode node = current.map.get(word.charAt(i));
+            if (node == null)
+                return false;
+            current = node;
+        }
+
+        return current.isEndOfTheWord;
+
+    }
+
 }
